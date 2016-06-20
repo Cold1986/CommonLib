0138f7b9027897cf8089addb664dfc54bd5ed70c
13935 <hl13935@ly.com>
2016-06-20 13:33:33 +0800

    验证码添加方法 自定义生成图片，返回Base64

--- Cold.CommonLibrary/VerifyCode/VerifyCodeHelper.cs
+++ Cold.CommonLibrary/VerifyCode/VerifyCodeHelper.cs
@@ -2,6 +2,8 @@
 using System.Web;
 using System.Drawing;
 using System.Security.Cryptography;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.IO;
 
 namespace Cold.CommonLibrary
 {
@@ -178,5 +180,76 @@ namespace Cold.CommonLibrary
             return destBmp;
         }
         #endregion
+
+        /// <summary>
+        /// 生产验证码
+        /// </summary>
+        /// <param name="checkCode">验证码</param>
+        /// <returns>返回Base64数据流</returns>
+        public static string CreateCheckCodeImage(string checkCode)
+        {
+            //判断字符串不等于空和null
+            if (checkCode == null || checkCode.Trim() == String.Empty)
+                return "";
+            //创建一个位图对象
+            System.Drawing.Bitmap image = new System.Drawing.Bitmap((int)Math.Ceiling((checkCode.Length * 12.5)), 22);
+            //创建Graphics对象
+            Graphics g = Graphics.FromImage(image);
+
+            try
+            {
+                //生成随机生成器
+                Random random = new Random();
+
+                //清空图片背景色
+                g.Clear(Color.White);
+
+                //画图片的背景噪音线
+                for (int i = 0; i < 2; i++)
+                {
+                    int x1 = random.Next(image.Width);
+                    int x2 = random.Next(image.Width);
+                    int y1 = random.Next(image.Height);
+                    int y2 = random.Next(image.Height);
+
+                    g.DrawLine(new Pen(Color.Black), x1, y1, x2, y2);
+                }
+
+                Font font = new System.Drawing.Font("Arial", 12, (System.Drawing.FontStyle.Bold));
+                System.Drawing.Drawing2D.LinearGradientBrush brush = new System.Drawing.Drawing2D.LinearGradientBrush(new Rectangle(0, 0, image.Width, image.Height), Color.Blue, Color.DarkRed, 1.2f, true);
+                g.DrawString(checkCode, font, brush, 2, 2);
+
+                //画图片的前景噪音点
+                for (int i = 0; i < 100; i++)
+                {
+                    int x = random.Next(image.Width);
+                    int y = random.Next(image.Height);
+
+                    image.SetPixel(x, y, Color.FromArgb(random.Next()));
+                }
+
+                //画图片的边框线
+                g.DrawRectangle(new Pen(Color.Silver), 0, 0, image.Width - 1, image.Height - 1);
+
+                //将图片输出到页面上
+                System.IO.MemoryStream ms = new System.IO.MemoryStream();
+                image.Save(ms, System.Drawing.Imaging.ImageFormat.Png);
+                byte[] b = ms.GetBuffer();
+                return Convert.ToBase64String(b);
+
+
+                //BinaryFormatter binFormatter = new BinaryFormatter();
+                //MemoryStream memStream = new MemoryStream();
+                //binFormatter.Serialize(memStream, image);
+                //byte[] bytes = memStream.GetBuffer();
+                //return Convert.ToBase64String(bytes);
+            }
+            finally
+            {
+                g.Dispose();
+                image.Dispose();
+            }
+
+        }
     }
 }
